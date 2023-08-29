086c3d024edc0663e44b06457bfefcfaeb2ba4ca
Nightenom <17338378+Nightenom@users.noreply.github.com>
2023-08-29 16:47:41 +0200

    Sprite 1.19.4 (#46)

--- src/main/java/com/ldtteam/blockui/controls/AbstractTextElement.java
+++ src/main/java/com/ldtteam/blockui/controls/AbstractTextElement.java
@@ -266,11 +266,6 @@ public abstract class AbstractTextElement extends Pane
         }
     }
 
-    protected int getTextRenderingColor(final double mx, final double my)
-    {
-        return enabled ? (isPointInPane(mx, my) ? textHoverColor : textColor) : textDisabledColor;
-    }
-
     @Override
     public void drawSelf(final PoseStack ms, final double mx, final double my)
     {
