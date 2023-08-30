1058efca88cb8cf6de4560b31834ad33f3a5d737
Nightenom <17338378+Nightenom@users.noreply.github.com>
2023-08-30 14:15:06 +0200

    Fix outofjar RL

--- src/main/java/com/ldtteam/blockui/util/resloc/OutOfJarResourceLocation.java
+++ src/main/java/com/ldtteam/blockui/util/resloc/OutOfJarResourceLocation.java
@@ -8,29 +8,28 @@ import net.minecraft.server.packs.resources.IoSupplier;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceMetadata;
-
+import org.jetbrains.annotations.Nullable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.UnaryOperator;
 
 public class OutOfJarResourceLocation extends ResourceLocation
 {
     private final Path nioPath;
 
-    protected OutOfJarResourceLocation(final String namespace, final Path path, final String pathString)
+    protected OutOfJarResourceLocation(final String namespace, final Path path, @Nullable final String pathString)
     {
-        super(namespace, pathString);
+        super(namespace,
+            pathString != null ? pathString : path.toString().toLowerCase().replace('\\', '/').replaceAll("[^a-z0-9/._-]", "_"));
         this.nioPath = path;
     }
 
     public static OutOfJarResourceLocation of(final String namespace, final Path path)
     {
-        final Path fullPath = path.toAbsolutePath().normalize();
-        return new OutOfJarResourceLocation(namespace,
-            fullPath,
-            fullPath.toString().toLowerCase().replace('\\', '/').replaceAll("[^a-z0-9/._-]", "_"));
+        return new OutOfJarResourceLocation(namespace, path, null);
     }
 
     @SuppressWarnings("resource")
@@ -60,9 +59,9 @@ public class OutOfJarResourceLocation extends ResourceLocation
 
     public static Resource getResourceHandle(final ResourceLocation resLoc, final ResourceManager fallbackManager)
     {
-        if (resLoc instanceof OutOfJarResourceLocation nioResLoc)
+        if (resLoc instanceof final OutOfJarResourceLocation nioResLoc)
         {
-            return fileExists(resLoc.withSuffix(".mcmeta"), fallbackManager) ?
+            return fileExists(nioResLoc.withSuffix(".mcmeta"), fallbackManager) ?
                 new OutOfJarResource(nioResLoc, FallbackResourceManager.convertToMetadata(() -> Files.newInputStream(nioResLoc.getNioPath()))) :
                 new OutOfJarResource(nioResLoc);
         }
@@ -85,6 +84,48 @@ public class OutOfJarResourceLocation extends ResourceLocation
             return Files.newBufferedReader(nioResLoc.nioPath);
         }
         return fallbackManager.openAsReader(resLoc);
+    }
+
+    /**
+     * @deprecated unsafe, use proper path methods!
+     */
+    @Override
+    @Deprecated(forRemoval = false)
+    public ResourceLocation withPath(final String path)
+    {
+        return of(getNamespace(), Path.of(path));
+    }
+
+    /**
+     * @deprecated unsafe, use proper path methods!
+     */
+    @Override
+    @Deprecated(forRemoval = false)
+    public ResourceLocation withPath(final UnaryOperator<String> op)
+    {
+        return of(getNamespace(), Path.of(op.apply(nioPath.toString())));
+    }
+
+    /**
+     * With path prefix (prefix + current path)
+     */
+    @Override
+    public ResourceLocation withPrefix(final String prefix)
+    {
+        return of(getNamespace(), Path.of(prefix).resolve(nioPath));
+    }
+
+    /**
+     * With both path and file suffix.
+     * <p>
+     * Tries to behave as vanilla method, so {@code withSuffix(".foo")} adds file suffix, unlike {@code path.resolve(".foo")} which
+     * would add file ".foo" in subdirectory. To get same behaviour as {@link Path#resolve(Path)} add '/' to start of parameter
+     */
+    @Override
+    public ResourceLocation withSuffix(final String suffix)
+    {
+        // in nio resolveSibling(...) = parent + path.of(...) so theoretically should resolve both correctly
+        return of(getNamespace(), nioPath.resolveSibling(nioPath.getFileName().toString() + suffix));
     }
 
     @Override
@@ -138,7 +179,7 @@ public class OutOfJarResourceLocation extends ResourceLocation
             super(null, () -> Files.newInputStream(resLoc.getNioPath()), metadataSupplier);
             this.resLoc = resLoc;
         }
-        
+
         public OutOfJarResource(final OutOfJarResourceLocation resLoc)
         {
             super(null, () -> Files.newInputStream(resLoc.getNioPath()));
@@ -149,7 +190,7 @@ public class OutOfJarResourceLocation extends ResourceLocation
         public PackResources source()
         {
             // currently only used at one place, so no-op should not crash
-            return null; 
+            return null;
         }
 
         @Override
