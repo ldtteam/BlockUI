af2ef1a3d4006acf0da127f3f87a524a5355e615
Nightenom <17338378+Nightenom@users.noreply.github.com>
2023-09-05 22:17:21 +0200

    jdoc

--- src/main/java/com/ldtteam/blockui/util/texture/IsOurTexture.java
+++ src/main/java/com/ldtteam/blockui/util/texture/IsOurTexture.java
@@ -4,6 +4,11 @@ import net.minecraft.client.renderer.texture.AbstractTexture;
 
 public final class IsOurTexture
 {
+    /**
+     * Mostly to deny loops in loading (since we load during rendering)
+     *
+     * @return true if given texture is from BlockUI
+     */
     public static final boolean isOur(final AbstractTexture texture)
     {
         return texture instanceof OutOfJarTexture || texture instanceof SpriteTexture || texture instanceof CursorTexture;
