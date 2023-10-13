25b02d170ba8fc58149561d7fcd7d266ad863f76
Nightenom <17338378+Nightenom@users.noreply.github.com>
2023-10-14 00:41:06 +0200

    Add NBT parsing to itemicon with optional overrides

--- src/main/java/com/ldtteam/blockui/BOGuiGraphics.java
+++ src/main/java/com/ldtteam/blockui/BOGuiGraphics.java
@@ -1,17 +1,35 @@
 package com.ldtteam.blockui;
 
+import com.ldtteam.blockui.util.SingleBlockNeighborhood;
 import com.ldtteam.blockui.util.cursor.Cursor;
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource.BufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.minecraftforge.client.model.data.ModelData;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix4f;
 
 public class BOGuiGraphics extends GuiGraphics
 {
+    // Static instance should be fine since gui rendering is on single thread
+    private static final SingleBlockNeighborhood NEIGHBORHOOD = new SingleBlockNeighborhood();
+
     private int cursorMaxDepth = -1;
     private Cursor selectedCursor = Cursor.DEFAULT;
 
@@ -65,5 +83,71 @@ public class BOGuiGraphics extends GuiGraphics
     public void applyCursor()
     {
         selectedCursor.apply();
+    }
+
+    /**
+     * Render given blockState with model just like {@link #renderItem(ItemStack, int, int)}
+     *
+     * @param blockState blockState
+     * @param modelData  model data for given blockState
+     * @param itemStack  backing itemStack for given blockState
+     */
+    public void renderBlockStateAsItem(final BlockState blockState, final ModelData modelData, final ItemStack itemStack)
+    {
+        final BakedModel itemModel = minecraft.getItemRenderer().getModel(itemStack, null, null, 0);
+        final boolean lighting = !itemModel.usesBlockLight();
+
+        pose().pushPose();
+        pose().translate(8, 8, 150);
+        pose().mulPoseMatrix(new Matrix4f().scaling(1.0F, -1.0F, 1.0F));
+        pose().scale(16.0F, 16.0F, 16.0F);
+        ForgeHooksClient.handleCameraTransforms(pose(), itemModel, ItemDisplayContext.GUI, false);
+        pose().translate(-0.5F, -0.5F, -0.5F);
+        pushPvmToShader();
+
+        if (lighting)
+        {
+            Lighting.setupForFlatItems();
+        }
+        else
+        {
+            Lighting.setupLevel(new Matrix4f());
+        }
+
+        minecraft.getBlockRenderer().renderSingleBlock(blockState, new PoseStack(), bufferSource(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, modelData, null);
+        flush();
+
+        final FluidState fluidState = blockState.getFluidState();
+        if (!fluidState.isEmpty())
+        {
+            final RenderType renderType = ItemBlockRenderTypes.getRenderLayer(fluidState);
+
+            NEIGHBORHOOD.blockState = blockState;
+            minecraft.getBlockRenderer().renderLiquid(BlockPos.ZERO, NEIGHBORHOOD, bufferSource().getBuffer(renderType), blockState, fluidState);
+
+            bufferSource().endBatch(renderType);
+        }
+
+        if (lighting)
+        {
+            Lighting.setupFor3DItems();
+        }
+
+        popPvmFromShader();
+        pose().popPose();
+    }
+
+    public void pushPvmToShader()
+    {
+        RenderSystem.getModelViewStack().pushPose();
+        RenderSystem.getModelViewStack().setIdentity();
+        RenderSystem.getModelViewStack().mulPoseMatrix(pose().last().pose());
+        RenderSystem.applyModelViewMatrix();
+    }
+
+    public void popPvmFromShader()
+    {
+        RenderSystem.getModelViewStack().popPose();
+        RenderSystem.applyModelViewMatrix();
     }
 }
--- src/main/java/com/ldtteam/blockui/controls/ItemIcon.java
+++ src/main/java/com/ldtteam/blockui/controls/ItemIcon.java
@@ -4,23 +4,36 @@ import com.ldtteam.blockui.BOGuiGraphics;
 import com.ldtteam.blockui.Pane;
 import com.ldtteam.blockui.PaneBuilders;
 import com.ldtteam.blockui.PaneParams;
+import com.ldtteam.blockui.mod.Log;
 import com.ldtteam.blockui.util.SpacerTextComponent;
 import com.ldtteam.blockui.util.ToggleableTextComponent;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.TagParser;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
@@ -37,6 +50,11 @@ public class ItemIcon extends Pane
      * ItemStack represented in the itemIcon.
      */
     private ItemStack itemStack;
+
+    /**
+     * BlockState + BlockEntity ModelData override
+     */
+    private BlockStateExtension blockStateExtension;
 
     /**
      * Standard constructor instantiating the itemIcon without any additional settings.
@@ -64,6 +82,20 @@ public class ItemIcon extends Pane
                 setItem(item.getDefaultInstance());
             }
         }
+
+        final String nbt = params.getString("nbt");
+        if (nbt != null && itemStack != null)
+        {
+            try
+            {
+                itemStack.setTag(TagParser.parseTag(nbt));
+                processBlockStateFromCurrentItemStack();
+            }
+            catch (final CommandSyntaxException e)
+            {
+                Log.getLogger().error("Cannot parse item nbt", e);
+            }
+        }
     }
 
     /**
@@ -74,6 +106,7 @@ public class ItemIcon extends Pane
     public void setItem(final ItemStack itemStackIn)
     {
         this.itemStack = itemStackIn;
+        processBlockStateFromCurrentItemStack();
         if (onHover instanceof final Tooltip tooltip)
         {
             tooltip.setTextOld(getModifiedItemStackTooltip());
@@ -90,6 +123,32 @@ public class ItemIcon extends Pane
         return this.itemStack;
     }
 
+    /**
+     * Overrides itemstack rendering with custom blockstate
+     * 
+     * @see #setBlockStateOverride(BlockState, ModelData) for BlockEntity model data
+     */
+    public void setBlockStateOverride(final BlockState blockState)
+    {
+        setBlockStateOverride(blockState, null);
+    }
+
+    /**
+     * Overrides itemstack rendering with custom blockstate
+     */
+    public void setBlockStateOverride(final BlockState blockState, final ModelData modelData)
+    {
+        setBlockStateOverride(blockState, modelData, true);
+    }
+
+    /**
+     * Overrides itemstack rendering with custom blockstate
+     */
+    public void setBlockStateOverride(final BlockState blockState, final ModelData modelData, final boolean renderItemDecorations)
+    {
+        this.blockStateExtension = new BlockStateExtension(blockState, modelData, renderItemDecorations);
+    }
+
     @Override
     public void drawSelf(final BOGuiGraphics target, final double mx, final double my)
     {
@@ -101,8 +160,19 @@ public class ItemIcon extends Pane
             ms.translate(x, y, 0.0f);
             ms.scale(this.getWidth() / DEFAULT_ITEMSTACK_SIZE, this.getHeight() / DEFAULT_ITEMSTACK_SIZE, 1.0f);
 
-            target.renderItem(itemStack, 0, 0);
-            target.renderItemDecorations(itemStack, 0, 0);
+            if (blockStateExtension == null)
+            {
+                target.renderItem(itemStack, 0, 0);
+            }
+            else
+            {
+                target.renderBlockStateAsItem(blockStateExtension.blockState, blockStateExtension.modelData(), itemStack);
+            }
+
+            if (blockStateExtension == null || blockStateExtension.renderItemDecorations)
+            {
+                target.renderItemDecorations(itemStack, 0, 0);
+            }
 
             RenderSystem.defaultBlendFunc();
             RenderSystem.disableBlend();
@@ -143,11 +213,13 @@ public class ItemIcon extends Pane
 
         if (tooltipFlags.advanced() && tooltipFlags.creative())
         {
+            // add tags
             ForgeRegistries.ITEMS.getHolder(itemStack.getItem())
                 .map(Holder::getTagKeys)
                 .ifPresent(tags -> tags
                     .forEach(tag -> result.add(1, wrapShift(Component.literal("#" + tag.location()).withStyle(ChatFormatting.DARK_PURPLE)))));
 
+            // add creative tabs
             int i = 1;
             for (final CreativeModeTab tab : CreativeModeTabRegistry.getSortedCreativeModeTabs())
             {
@@ -156,6 +228,25 @@ public class ItemIcon extends Pane
                     result.add(i++, wrapShift(tab.getDisplayName().copy().withStyle(ChatFormatting.BLUE)));
                 }
             }
+
+            // add blockstate info
+            if (blockStateExtension != null)
+            {
+                result.add(wrapShift(Component.empty()));
+                result.add(wrapShift(Component.translatable("blockui.tooltip.properties")));
+
+                final BlockState blockState = blockStateExtension.blockState;
+                for (final Property<?> property : blockState.getProperties())
+                {
+                    result.add(wrapShift(Component.literal("  " + property.getName() + " = " + getValueName(blockState, property)).withStyle(ChatFormatting.GRAY)));
+                }
+            }
+
+            // add "show more info" text
+            result.add(ToggleableTextComponent.ofNegated(Screen::hasShiftDown, Component.empty()));
+            result.add(ToggleableTextComponent.ofNegated(Screen::hasShiftDown,
+                Component.translatable("blockui.tooltip.item_additional_info", Component.translatable("key.keyboard.left.shift"))
+                    .withStyle(ChatFormatting.GOLD)));
         }
 
         result.add(1, FIX_VANILLA_TOOLTIP);
@@ -165,5 +256,81 @@ public class ItemIcon extends Pane
     private static MutableComponent wrapShift(final MutableComponent wrapped)
     {
         return ToggleableTextComponent.of(Screen::hasShiftDown, wrapped);
+    }
+
+    /**
+     * Extracts blockstate/modeldata from current itemstack
+     */
+    protected void processBlockStateFromCurrentItemStack()
+    {
+        // remove old extension
+        blockStateExtension = null;
+
+        if (!(itemStack.getItem() instanceof final BlockItem blockItem))
+        {
+            return;
+        }
+    
+        BlockState blockstate = blockItem.getBlock().defaultBlockState();
+
+        // parse block state
+        final CompoundTag blockStateTag = itemStack.getTagElement(BlockItem.BLOCK_STATE_TAG);
+
+        if (blockStateTag == null)
+        {
+            return;
+        }
+
+        final StateDefinition<Block, BlockState> statedefinition = blockstate.getBlock().getStateDefinition();
+
+        for (final String propertyKey : blockStateTag.getAllKeys())
+        {
+            final Property<?> property = statedefinition.getProperty(propertyKey);
+            if (property != null)
+            {
+                blockstate = updateState(blockstate, property, blockStateTag.getString(propertyKey));
+            }
+        }
+
+        // try parsing blockentity
+        final CompoundTag blockEntityTag = itemStack.getTagElement(BlockItem.BLOCK_ENTITY_TAG);
+        ModelData modelData = null;
+        if (blockEntityTag != null)
+        {
+            try
+            {
+                // use probably invalid pos
+                final BlockEntity be = BlockEntity.loadStatic(BlockPos.ZERO.below(1000), blockstate, blockEntityTag);
+                if (be != null)
+                {
+                    modelData = be.getModelData();
+                }
+            }
+            catch (Exception e)
+            {
+                Log.getLogger().warn("Error while parsing blockentity data", e);
+            }
+        }
+
+        blockStateExtension = new BlockStateExtension(blockstate, modelData, true);
+    }
+
+    private static <T extends Comparable<T>> BlockState updateState(final BlockState state, final Property<T> property, final String valueName)
+    {
+        return property.getValue(valueName).map(value -> state.setValue(property, value)).orElse(state);
+    }
+
+    private static <T extends Comparable<T>> String getValueName(final BlockState blockState, final Property<T> property)
+    {
+        return property.getName(blockState.getValue(property));
+    }
+
+    public static record BlockStateExtension(BlockState blockState, @Nullable ModelData modelData, boolean renderItemDecorations)
+    {
+        @Override
+        public ModelData modelData()
+        {
+            return modelData == null ? ModelData.EMPTY : modelData;
+        }
     }
 }
