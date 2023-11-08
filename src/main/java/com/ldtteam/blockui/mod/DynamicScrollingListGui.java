d42516c7c034b455216b737bf352711380d41092
Thom van den Akker <thodor20@gmail.com>
2023-11-08 14:31:29 +0100

    Allow scrolling list items to have a dynamic size

---
+++ src/main/java/com/ldtteam/blockui/mod/DynamicScrollingListGui.java
@@ -0,0 +1,56 @@
+package com.ldtteam.blockui.mod;
+
+import com.ldtteam.blockui.Pane;
+import com.ldtteam.blockui.controls.Text;
+import com.ldtteam.blockui.util.records.SizeI;
+import com.ldtteam.blockui.views.BOWindow;
+import com.ldtteam.blockui.views.ScrollingList;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Sets up gui for dynamic scrolling lists.
+ */
+public class DynamicScrollingListGui
+{
+    public static void setup(final BOWindow window)
+    {
+        final ScrollingList list1 = window.findPaneOfTypeByID("list1", ScrollingList.class);
+        list1.setDataProvider(new ScrollingList.DataProvider()
+        {
+            @Override
+            public int getElementCount()
+            {
+                return 10;
+            }
+
+            @Override
+            public void updateElement(final int index, final Pane rowPane)
+            {
+                rowPane.findPaneByType(Text.class).setText(Component.literal("Hi " + index));
+            }
+        });
+
+        final ScrollingList list2 = window.findPaneOfTypeByID("list2", ScrollingList.class);
+        list2.setDataProvider(new ScrollingList.DataProvider()
+        {
+            @Override
+            public int getElementCount()
+            {
+                return 20;
+            }
+
+            @Override
+            public @Nullable SizeI getElementSize(final int index, final SizeI oldSize)
+            {
+                return index % 2 == 0 ? new SizeI(100, 40) : null;
+            }
+
+            @Override
+            public void updateElement(final int index, final Pane rowPane)
+            {
+                rowPane.findPaneByType(Text.class).setText(Component.literal("Hi " + index));
+            }
+        });
+    }
+}
