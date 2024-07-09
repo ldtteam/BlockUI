f4600e9ee7eac0cd473b568cad3038b5e7eb1ab2
Nightenom <17338378+Nightenom@users.noreply.github.com>
2024-07-09 14:51:49 +0200

    Bump forge, move messages to StreamCodec, add some codecs for structurize

--- src/main/java/com/ldtteam/common/network/PlayMessageType.java
+++ src/main/java/com/ldtteam/common/network/PlayMessageType.java
@@ -17,11 +17,12 @@ import java.util.function.BiFunction;
  * Class to connect message type with proper sided registration.
  */
 public record PlayMessageType<T extends AbstractUnsidedPlayMessage>(Type<T> id,
-    BiFunction<RegistryFriendlyByteBuf, PlayMessageType<T>, T> messageFactory,
+    StreamCodec<RegistryFriendlyByteBuf, T> codec,
     boolean allowNullPlayer,
     @Nullable PayloadAction<T, Player> client,
     @Nullable PayloadAction<T, ServerPlayer> server)
 {
+    // =============== MESSAGE VARIANTS ===============
 
     /**
      * Creates type for Server (sender) -> Client (receiver) message
@@ -65,7 +66,7 @@ public record PlayMessageType<T extends AbstractUnsidedPlayMessage>(Type<T> id,
         final boolean playerNullable,
         final boolean executeOnNetworkThread)
     {
-        return new PlayMessageType<>(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
+        return codecise(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
             messageFactory,
             playerNullable,
             threadRedirect(AbstractClientPlayMessage::onExecute, executeOnNetworkThread),
@@ -84,7 +85,7 @@ public record PlayMessageType<T extends AbstractUnsidedPlayMessage>(Type<T> id,
         final boolean playerNullable,
         final boolean executeOnNetworkThread)
     {
-        return new PlayMessageType<>(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
+        return codecise(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
             messageFactory,
             playerNullable,
             null,
@@ -103,8 +104,97 @@ public record PlayMessageType<T extends AbstractUnsidedPlayMessage>(Type<T> id,
         final boolean playerNullable,
         final boolean executeOnNetworkThread)
     {
-        return new PlayMessageType<>(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
+        return codecise(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
             messageFactory,
+            playerNullable,
+            threadRedirect(AbstractPlayMessage::onClientExecute, executeOnNetworkThread),
+            threadRedirect(AbstractPlayMessage::onServerExecute, executeOnNetworkThread));
+    }
+
+    // =============== CODEC VARIANTS ===============
+
+    /**
+     * Creates type for Server (sender) -> Client (receiver) message
+     */
+    public static <T extends AbstractClientPlayMessage> PlayMessageType<T> forClient(final String modId,
+        final String messageName,
+        final StreamCodec<RegistryFriendlyByteBuf, T> codec)
+    {
+        return forClient(modId, messageName, codec, false, false);
+    }
+
+    /**
+     * Creates type for Client (sender) -> Server (receiver) message
+     */
+    public static <T extends AbstractServerPlayMessage> PlayMessageType<T> forServer(final String modId,
+        final String messageName,
+        final StreamCodec<RegistryFriendlyByteBuf, T> codec)
+    {
+        return forServer(modId, messageName, codec, false, false);
+    }
+
+    /**
+     * Creates type for bidirectional message
+     */
+    public static <T extends AbstractPlayMessage> PlayMessageType<T> forBothSides(final String modId,
+        final String messageName,
+        final StreamCodec<RegistryFriendlyByteBuf, T> codec)
+    {
+        return forBothSides(modId, messageName, codec, false, false);
+    }
+
+    /**
+     * Creates type for Server (sender) -> Client (receiver) message
+     *
+     * @param playerNullable         if false then message wont execute without player
+     * @param executeOnNetworkThread if true will execute on logical side main thread
+     */
+    public static <T extends AbstractClientPlayMessage> PlayMessageType<T> forClient(final String modId,
+        final String messageName,
+        final StreamCodec<RegistryFriendlyByteBuf, T> codec,
+        final boolean playerNullable,
+        final boolean executeOnNetworkThread)
+    {
+        return new PlayMessageType<>(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
+            codec,
+            playerNullable,
+            threadRedirect(AbstractClientPlayMessage::onExecute, executeOnNetworkThread),
+            null);
+    }
+
+    /**
+     * Creates type for Client (sender) -> Server (receiver) message
+     *
+     * @param playerNullable         if false then message wont execute without player
+     * @param executeOnNetworkThread if true will execute on logical side main thread
+     */
+    public static <T extends AbstractServerPlayMessage> PlayMessageType<T> forServer(final String modId,
+        final String messageName,
+        final StreamCodec<RegistryFriendlyByteBuf, T> codec,
+        final boolean playerNullable,
+        final boolean executeOnNetworkThread)
+    {
+        return new PlayMessageType<>(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
+            codec,
+            playerNullable,
+            null,
+            threadRedirect(AbstractServerPlayMessage::onExecute, executeOnNetworkThread));
+    }
+
+    /**
+     * Creates type for bidirectional message
+     *
+     * @param playerNullable         if false then message wont execute without player
+     * @param executeOnNetworkThread if true will execute on logical side main thread
+     */
+    public static <T extends AbstractPlayMessage> PlayMessageType<T> forBothSides(final String modId,
+        final String messageName,
+        final StreamCodec<RegistryFriendlyByteBuf, T> codec,
+        final boolean playerNullable,
+        final boolean executeOnNetworkThread)
+    {
+        return new PlayMessageType<>(new Type<>(ResourceLocation.fromNamespaceAndPath(modId, messageName)),
+            codec,
             playerNullable,
             threadRedirect(AbstractPlayMessage::onClientExecute, executeOnNetworkThread),
             threadRedirect(AbstractPlayMessage::onServerExecute, executeOnNetworkThread));
@@ -128,8 +218,6 @@ public record PlayMessageType<T extends AbstractUnsidedPlayMessage>(Type<T> id,
      */
     public void register(final PayloadRegistrar registry)
     {
-        final StreamCodec<RegistryFriendlyByteBuf, T> codec =
-            StreamCodec.of((buf, msg) -> msg.toBytes(buf), buf -> messageFactory.apply(buf, this));
         if (client != null && server != null)
         {
             registry.playBidirectional(id, codec, this::onBidirectional);
@@ -198,5 +286,46 @@ public record PlayMessageType<T extends AbstractUnsidedPlayMessage>(Type<T> id,
     private interface PayloadAction<T, U>
     {
         void handle(T payload, IPayloadContext context, U player);
+    }
+
+    /**
+     * Redirect our messages to {@link StreamCodec}
+     */
+    private static <T extends AbstractUnsidedPlayMessage> PlayMessageType<T> codecise(Type<T> id,
+        BiFunction<RegistryFriendlyByteBuf, PlayMessageType<T>, T> messageFactory,
+        boolean allowNullPlayer,
+        @Nullable PayloadAction<T, Player> client,
+        @Nullable PayloadAction<T, ServerPlayer> server)
+    {
+        final MessageStreamCodec<T> codec = new MessageStreamCodec<>(messageFactory);
+        final PlayMessageType<T> type = new PlayMessageType<>(id, codec, allowNullPlayer, client, server);
+        codec.type = type;
+        return type;
+    }
+
+    /**
+     * Codec for wrapping our messages
+     */
+    private static class MessageStreamCodec<T extends AbstractUnsidedPlayMessage> implements StreamCodec<RegistryFriendlyByteBuf, T>
+    {
+        private final BiFunction<RegistryFriendlyByteBuf, PlayMessageType<T>, T> messageFactory;
+        private PlayMessageType<T> type;
+
+        private MessageStreamCodec(BiFunction<RegistryFriendlyByteBuf, PlayMessageType<T>, T> messageFactory)
+        {
+            this.messageFactory = messageFactory;
+        }
+
+        @Override
+        public T decode(RegistryFriendlyByteBuf buf)
+        {
+            return messageFactory.apply(buf, type);
+        }
+
+        @Override
+        public void encode(RegistryFriendlyByteBuf buf, T msg)
+        {
+            msg.toBytes(buf);
+        }
     }
 }
