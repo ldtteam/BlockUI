e3af9ff1d1529824cbe02854af35b43b803628a2
Nightenom <17338378+Nightenom@users.noreply.github.com>
2024-08-20 15:10:50 +0200

    Create ClientConfigHelper.java

---
+++ src/main/java/com/ldtteam/common/config/ClientConfigHelper.java
@@ -0,0 +1,16 @@
+package com.ldtteam.common.config;
+
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+/**
+ * Client bouncer class
+ */
+public class ClientConfigHelper
+{
+    static registerClient(final ModContainer modContainer)
+    {
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+}
